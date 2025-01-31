e886b9b1a15238b8acf061b26eb1a44efc191627
jeppepro <35989433+jeppepro@users.noreply.github.com>
2025-01-31 11:46:59 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,6 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "app/**/*.{ts,tsx}",
-    "components/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
@@ -53,5 +51,14 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
+  safelist: [
+    "hover:bg-[#0030F0]",
+    "active:bg-[#002CDB]",
+    "active:text-[#DBE2FF]",
+    "hover:text-[#1141ff]",
+    "hover:bg-[#DDE4FD]",
+    "active:bg-[#CDD5F9]",
+    "active:text-[#2F53E1]",
+  ],
 }
 
