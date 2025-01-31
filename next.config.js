ed18f0239f65b412ab0500102e37c2035b898e0c
jeppepro <35989433+jeppepro@users.noreply.github.com>
2025-01-31 13:14:08 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
-/*
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
-*/
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== "development",
-  },
-  experimental: {
-    optimizeCss: true,
-    optimizeImages: true,
-  },
 }
 
 module.exports = nextConfig
