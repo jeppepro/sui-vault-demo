e2040dfa2540fc1aec3da61e8d912137873c74c5
jeppepro <35989433+jeppepro@users.noreply.github.com>
2025-01-31 13:13:30 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,7 @@
-/* import "../app/globals.css"
+import "../app/globals.css"
 import type { AppProps } from "next/app"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
-}
-*/
-import "../app/globals.css"
-import type { AppProps } from "next/app"
-import { LazyMotion, domAnimation } from "framer-motion"
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <LazyMotion features={domAnimation}>
-      <Component {...pageProps} />
-    </LazyMotion>
-  )
 }
 
